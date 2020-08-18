4c96e840c31867bbc5164c5f2de3f2e01d548d5b
asaucedo777@gmail.com <asaucedo777@gmail.com>
2020-08-18 13:01:36 +0200

    Initial commit

---
+++ jarduino.sh
@@ -0,0 +1,7 @@
+echo "# jarduino-android" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/asaucedo777/jarduino-android.git
+git push -u origin master
+
